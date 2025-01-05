3b60fdd03c44fa47697584ea52c9b51b8d7bd5d6
donguri-k <innocent.dyslexia@gmail.com>
2025-01-05 21:19:21 +0900

    Add files via upload

--- script/arena_improver.js
+++ script/arena_improver.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         donguri Arena Improver
-// @version      0.8a
+// @version      0.9a
 // @description  fix arena ui
 // @author       7234e634
 // @match        https://donguri.5ch.net/teambattle
@@ -8,6 +8,8 @@
 
 
 (()=>{
+  const vw = Math.min(document.documentElement.clientWidth, window.innerWidth || 0);
+
   const topbar = document.createElement('div');
   topbar.style.width = '100%';
   topbar.style.height = '48px';
@@ -26,6 +28,36 @@
   refreshButton.textContent = '陣地更新';
   refreshButton.addEventListener('click',refreshAreaInfo);
   refreshButton.style.marginLeft = '2px';
+
+  const sortSelect = document.createElement('select');
+  sortSelect.style.maxWidth = '128px';
+  let lastSelectedValue = sortSelect.value;
+
+  (()=>{
+    const option1 = document.createElement('option');
+    const option2 = document.createElement('option');
+    option1.textContent = 'デフォルト順';
+    option2.textContent = '装備制限順';
+    option1.value = 'default';
+    option2.value = 'cond';
+    sortSelect.append(option1,option2);
+
+    sortSelect.addEventListener('change', handleSelection);
+
+    // 再選択を可能にする
+    sortSelect.addEventListener('focus', () => lastSelectedValue = null);
+    sortSelect.addEventListener('mousedown', handleSelection);
+    sortSelect.addEventListener('keydown', (e) => {
+      if (e.key === 'Enter' || e.key === ' ') handleSelection();
+    });
+    function handleSelection() {
+      const currentValue = sortSelect.value;
+      if (lastSelectedValue !== currentValue || lastSelectedValue === null) {
+        sortCells(currentValue);
+        lastSelectedValue = currentValue;
+      }
+    }
+  })();
 
   const arenaField = document.createElement('dialog');
   arenaField.style.position = 'fixed';
@@ -51,11 +83,38 @@
     closeButton.addEventListener('click', ()=>{arenaField.close()});
     const table = document.createElement('table');
     arenaField.append(closeButton,table);
-  })()
-  const arenaResult = document.createElement('div');
-  arenaResult.style.position = 'absolute';
+  })();
 
-  topbar.append(refreshButton, cellButton);
+  const arenaResult = document.createElement('dialog');
+  arenaResult.style.position = 'fixed';
+  arenaResult.style.width = '50%';
+  arenaResult.style.bottom = '10px';
+  arenaResult.style.left = 'auto';
+  arenaResult.style.height = '640px';
+  arenaResult.style.background = '#fff';
+  arenaResult.style.color = '#000';
+  arenaResult.style.fontSize = '80%';
+  arenaResult.style.border = 'solid 1px #000';
+  arenaResult.style.margin = '0';
+  arenaResult.style.textAlign = 'left';
+  arenaResult.style.overflowY = 'auto';
+  window.addEventListener('click', (event) => {
+    if (!arenaResult.contains(event.target)) {
+      arenaResult.close();
+    }
+  });
+  arenaField.append(arenaResult);
+  (()=>{
+    if (vw < 768) {
+      sortSelect.style.fontSize = '60%';
+      cellButton.style.fontSize = '60%';
+      refreshButton.style.fontSize = '60%';
+    }
+    const div = document.createElement('div');
+    div.style.display = 'inline-block';
+    div.append(refreshButton, cellButton);
+    topbar.append(sortSelect, div);
+  })();
   document.body.append(topbar,arenaField);
 
   const grid = document.querySelector('.grid');
@@ -64,7 +123,8 @@
 
   const table = document.querySelector('table');
   table.parentNode.style.maxWidth = '100%';
-  table.parentNode.style.overflowX = 'auto';
+  table.parentNode.style.overflow = 'auto';
+  table.parentNode.style.height = '60vh';
 
   function scaleContentsToFit(container, contents){
     const containerWidth = container.clientWidth;
@@ -140,16 +200,13 @@
       })
     }).catch(e=>console.error(e))
   }
-  function getArenaInfo(){
-    refreshAreaInfo();
-    let br = document.createElement('br');
+  async function getArenaInfo(){
+    await refreshAreaInfo();
     grid.style.gridTemplateRows = grid.style.gridTemplateRows.replace('35px','65px'),
     grid.style.gridTemplateColumns = grid.style.gridTemplateColumns.replace('35px','105px'),
     grid.parentNode.style.height = null,
     grid.parentNode.style.padding = '20px 0';
     const cols = Number(grid.style.gridTemplateColumns.match(/repeat\((\d+),/)[1]);
-    const vw = Math.min(document.documentElement.clientWidth, window.innerWidth || 0);
-  console.log(cols, vw);
     if (vw < 768 && cols > 8) {
       grid.style.gridTemplateColumns = 'repeat(8, 105px)';
     }
@@ -162,16 +219,21 @@
       .then(res=>
         res.ok?res.text():Promise.reject('res.ng')
       )
-      .then(text=>{
+      .then(text => {
         let doc = new DOMParser().parseFromString(text,'text/html'),
         h1 = doc?.querySelector('h1')?.textContent;
         if(h1 !== 'どんぐりチーム戦い') return Promise.reject(`title.ng [${row}][${col}][${h1}]`);
         let cond = doc.querySelector('small')?.textContent || '';
         if(!cond) return Promise.reject(`cond.ng [${row}][${col}][${h1}]`);
         let holder = doc.querySelector('strong')?.textContent || '',
-        shortenCond = cond.replace('| [エリート]','e').replace('から','-').replace(/(まで|\[|\]|\||\s)/g,''),
+        shortenCond = cond.replace('| [エリート]','e').replace('から','-').replace(/(まで|\[|\]|\||\s)/g,'');
+        const p = [document.createElement('p'), document.createElement('p')];
+        p[0].textContent = shortenCond;
+        p[1].textContent = holder;
+        p[0].style.margin = '0';
+        p[1].style.margin = '0';
         cell = elm.cloneNode();
-        cell.append(shortenCond,br.cloneNode(),holder),
+        cell.append(p[0],p[1]),
         cell.style.overflow = 'hidden',
         cell.style.width = '100px',
         cell.style.height = '60px',
@@ -185,22 +247,101 @@
             if(h1 !== 'どんぐりチーム戦い') return Promise.reject(`title.ng`);
             const table = doc.querySelector('table');
             if(!table) return Promise.reject(`table.ng`);
-            // table.style.transform = 'scale(0.8)';
-            // table.style.transformOrigin = 'top left';
+            table.style.marginLeft = '-18px';
             arenaField.querySelector('table').replaceWith(table);
             scaleContentsToFit(arenaField,table);
             const forms = table.querySelectorAll('form');
-            forms.forEach(form => {
-              form.target = '_new';
+            forms[0].addEventListener('submit', (event) => {
+              event.preventDefault();
+              const formData = new FormData(forms[0]);
+              arenaChallenge(formData);
             })
+            forms[1].target = '_blank';
+            forms[2].target = '_blank';
           })
           arenaField.show();
         });
-        elm.replaceWith(cell)}
-      ).catch(e=>console.error(e))
-      }
-    )
-
+        elm.replaceWith(cell);
+      })
+      .catch(e=>console.error(e))
+    })
     scaleContentsToFit(grid.parentNode, grid);
+  }
+
+  async function arenaChallenge (formData){
+    const options = {
+      method: 'POST',
+      body: formData,
+    };
+    try {
+      const response = await fetch('/teamchallenge', options);
+      if(!response.ok){
+        throw new Error('/teamchallenge res.ng');
+      }
+      const text = await response.text();
+      arenaResult.innerText = text;
+      arenaResult.show();
+    } catch (e) {
+      arenaResult.innerText = e;
+      arenaResult.show();
+    }
+
+  }
+  function sortCells(type){
+    const cells = [...document.querySelectorAll('.cell')];
+    if(type === 'default') {
+      cells.sort((a, b) => {
+        const rowA = a.dataset.row;
+        const rowB = b.dataset.row;
+        const colA = a.dataset.col;
+        const colB = b.dataset.col;
+        return rowA - rowB || colA - colB;
+      })
+    }
+
+    if(type === 'cond') {
+      cells.sort((a, b) => {
+        const condA = a.querySelector('p')?.textContent;
+        const condB = b.querySelector('p')?.textContent;
+        if(!condA || !condB) return;
+        const splitA = condA.split('-');
+        const splitB = condB.split('-');
+        
+        const isCompositeA = splitA.length > 1;
+        const isCompositeB = splitB.length > 1;
+        
+        const order = ['N','R','SR','SSR','UR'];
+        // '-'を含むものを後 
+        if (isCompositeA !== isCompositeB) return isCompositeA - isCompositeB;
+        if (isCompositeA) {
+          // 後のランク優先で比較
+          const backA = splitA[1];
+          const backB = splitB[1];
+          const indexBackA = order.indexOf(backA);
+          const indexBackB = order.indexOf(backB);
+          if (indexBackA !== indexBackB) return indexBackA - indexBackB;
+          
+          const frontA = splitA[0];
+          const frontB = splitB[0];
+          const indexFrontA = order.indexOf(frontA);
+          const indexFrontB = order.indexOf(frontB);
+          return indexFrontA - indexFrontB;
+        }
+        // 単一
+        const baseA = condA.replace(/だけ|e/g, '');
+        const baseB = condB.replace(/だけ|e/g, ''); 
+        const indexA = order.indexOf(baseA);
+        const indexB = order.indexOf(baseB);
+        
+        if (indexA !== indexB) return indexA - indexB;
+        
+        const flag = s => 
+          (s.includes('だけ') ? 1 : 0) + (s.includes('e') ? 2 : 0);
+        
+        return flag(condA) - flag(condB);
+      });
+    }
+    grid.innerHTML = '';
+    cells.forEach(cell => grid.append(cell));
   }
 })();
